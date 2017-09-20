69854c3184e5d43254d29bc8b67e8e41b55fcd87
Tenzin Dendup <tenzin.dendup@gmail.com>
2017-09-20 19:25:40 +1000

    updated web assignment

---
+++ semester2/WDP/Canberra_Restaurants/Canberra_Restaurants/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Canberra_Restaurants.Models;
+
+namespace Canberra_Restaurants.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public IActionResult Restaurants()
+		{
+
+			return View();
+		}
+
+		public IActionResult Cuisine()
+		{
+
+			return View();
+		}
+
+		public IActionResult Dishes()
+		{
+
+			return View();
+		}
+
+		public IActionResult Prices()
+		{
+
+			return View();
+		}
+
+		public IActionResult Location()
+		{
+
+			return View();
+		}
+
+		public IActionResult Reviews()
+		{
+
+			return View();
+		}
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
