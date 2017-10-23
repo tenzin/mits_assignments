24776e78328457dfd57c50828923251dddaa29cc
Tenzin Dendup <tenzin.dendup@gmail.com>
2017-10-23 22:27:30 +1100

    updated ST1 and SSIT assignments

---
+++ semester2/ST1/Assignment1/MainClass.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class MainClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        GUIClass gui = new GUIClass();
+    }
+}
